6c0c79abda12bf5d0872c343aa740870e5f642fe
Will B <will1b@users.noreply.github.com>
2018-05-30 21:04:17 +0100

    Add support for command-line arguments

--- Taiga2MAL.py
+++ Taiga2MAL.py
@@ -5,12 +5,25 @@ import lxml.etree as ET
 import datetime
  
 # Required to actually fetch the APPDATA directory
+# We don't import all of os just because it has some dodgy methods
 from os import getenv
 from os.path import join
- 
-# Change this if you're using portable Taiga or something.
-DATA_DIRECTORY = join(getenv('APPDATA'), "Taiga", "data")
- 
+
+# Allow people to inject their own arguments
+import argparse
+
+# Default data directory is the default installation directory
+DEFAULT_DATA_DIRECTORY = join(getenv('APPDATA'), "Taiga", "data")
+
+parser = argparse.ArgumentParser(
+    description="Export Taiga XML file in a MAL export compatible format.")
+
+parser.add_argument("-u", "--user", '--username', dest='username',
+                    type=str, default=None,
+                    help = "your MAL username")
+parser.add_argument("-d", "--dir", '--directory',dest='data_directory',
+                    default=DEFAULT_DATA_DIRECTORY,
+                    help="your '<TAIGA_HOME>/data' directory")
  
 def convert_status(status):
     new_status = "Watching"  # Safe default
@@ -70,13 +83,9 @@ def parse_no_meta(path):
     return ET.fromstring("".join(lines))
  
  
-# We need anime.xml for extracting titles and episode counts
-db_tree = parse_no_meta(join(DATA_DIRECTORY, "db", "anime.xml"))
- 
- 
 # Returns a tuple of title, type, episode_count
 # Exactly the format needed later
-def lookup_anime(anime_id):
+def lookup_anime(db_tree, anime_id):
     element = db_tree.find("./anime[id='{0}']".format(anime_id))
     
     # Title is already a CDATA, but we'll just reconstruct
@@ -94,10 +103,22 @@ def lookup_anime(anime_id):
  
  
 def main():
+    # Get arguments
+    args = parser.parse_args()
+
+    username = args.username
+    if username is None:
+        username = input("What is your MAL username? "
+              "(required to look in the right directory)\n")
+    
+    data_directory = args.data_directory
+
+    # We need anime.xml for extracting titles and episode counts
+    db_tree = parse_no_meta(join(data_directory, "db", "anime.xml"))
+    
     # Locate anime.xml based on the root directory
-    username = input("What is your MAL username? "
-                     "(required to look in the right directory)\n")
-    path = join(DATA_DIRECTORY, "user", username + "@myanimelist", "anime.xml")
+                    
+    path = join(data_directory, "user", username + "@myanimelist", "anime.xml")
  
     root = parse_no_meta(path)
  
@@ -113,7 +134,6 @@ def main():
  
         # Adjust for Taiga storing scores out of 100
         raw_score = anime.find('score').text
-        # Requires initial cast of Element to str
         score = str(int(raw_score) // 10)
  
         status = convert_status(anime.find('status').text)
@@ -122,7 +142,7 @@ def main():
         rewatching_ep = anime.find('rewatching_ep').text
  
         # Fetch required information from the Taiga anime db
-        title, anime_type, episode_count = lookup_anime(anime_id)
+        title, anime_type, episode_count = lookup_anime(db_tree, anime_id)
  
         # CDATA types
         tags = anime.find('tags').text
@@ -132,31 +152,18 @@ def main():
         newanime = ET.SubElement(root2, 'anime')
  
         build_SubElement(newanime, 'series_animedb_id', anime_id)
- 
         build_SubElement(newanime, 'series_title', make_CDATA(title))
- 
         build_SubElement(newanime, 'series_type', anime_type)
- 
         build_SubElement(newanime, 'series_episodes', episode_count)
- 
         build_SubElement(newanime, 'my_watched_episodes', progress)
- 
         build_SubElement(newanime, 'my_start_date', start_date)
- 
         build_SubElement(newanime, 'my_finish_date', end_date)
- 
         build_SubElement(newanime, 'my_score', score)
- 
         build_SubElement(newanime, 'my_status', status)
- 
         build_SubElement(newanime, 'my_comments', make_CDATA(notes))
- 
         build_SubElement(newanime, 'my_times_watched', rewatched_times)
- 
         build_SubElement(newanime, 'my_rewatching', rewatching)
- 
         build_SubElement(newanime, 'my_rewatching_ep', rewatching_ep)
- 
         build_SubElement(newanime, 'my_tags', make_CDATA(tags))
  
         # Fields with default parameters
